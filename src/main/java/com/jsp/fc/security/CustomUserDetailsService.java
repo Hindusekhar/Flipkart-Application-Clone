e4a6977ca34390148b6a177f096dc7225a23e7ff
Hindusekhar <hindusekhar2002@gmail.com>
2024-02-08 10:50:00 +0530

    upated security

--- src/main/java/com/jsp/fc/security/CustomUserDetailsService.java
+++ src/main/java/com/jsp/fc/security/CustomUserDetailsService.java
@@ -4,12 +4,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
 import com.jsp.fc.exception.UserNotADMINException;
 import com.jsp.fc.repository.UserRepository;
 
 import lombok.AllArgsConstructor;
-
+@Service
 @AllArgsConstructor
 public class CustomUserDetailsService implements UserDetailsService{
 
